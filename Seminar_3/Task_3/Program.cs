d86e4bb7acde467cf42d2ef35d79ee69448f8255
Dmitry Gurkin <mrack2k@gmail.com>
2023-02-10 02:15:05 +0300

    Edit Task_3 for Seminar3

--- Seminar_3/Task_3/Program.cs
+++ Seminar_3/Task_3/Program.cs
@@ -3,3 +3,13 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.Clear();
+System.Console.Write("Введите целое число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++) 
+{
+    System.Console.WriteLine("Куб числа " + i + " равен " + Math.Pow(i,3));
+
+}
+
+
