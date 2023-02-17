d1cd5aef0bd827bd01c45abd0b3e48d978be7d2c
Dmitry Gurkin <mrack2k@gmail.com>
2023-02-18 01:40:44 +0300

    Edit Task 1-2 for Seminar6

--- Seminar_6/Task_1/Program.cs
+++ Seminar_6/Task_1/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int InputNumber(string str)
+{
+    int num;
+    string? text;
+    while(true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out num))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено не корректное число, попробуйте еще раз!");
+    }
+    return num;
+}
+
+void PrintArray (int[] a)
+{
+    System.Console.WriteLine("[" + string.Join(", ", a) + "]");
+}
+
+int FindNumb (int[] number)
+{
+    int count = 0;
+    for (int i = 0; i < number.Length; i++)
+        {
+            if (number[i] > 0) count++;   
+        }
+    return count;
+}
+
+Console.Clear();
+int a = InputNumber("Сколько чисел будем вводить? -> "); 
+int[] inputNum = new int[a];
+for (int i = 0; i < a; i++)
+    {
+        inputNum[i] = InputNumber("Введите число -> ");
+    }
+Console.Clear();
+Console.WriteLine("Вы ввели следующие числа: ");
+PrintArray(inputNum);
+System.Console.WriteLine("Количество чисел больше 0: " + FindNumb(inputNum));
+
+
--- Seminar_6/Task_2/Program.cs
+++ Seminar_6/Task_2/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Решаем систему. Находим  x, а потом и y.
+// k1 * x + b1 = k2 * x + b2
+// k1 * x - k2 * x = b2 - b1
+// x(k1-k2) = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+
+int InputNumber(string str)
+{
+    int num;
+    string? text;
+    while(true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out num))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено не корректное число, попробуйте еще раз!");
+    }
+    return num;
+}
+
+void CrossPoint (double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k2 * x + b2;
+    System.Console.Write("Точка пересечения двух прямых находится в координатах: ");
+    System.Console.Write("X: " + x);
+    System.Console.WriteLine(", Y: " + y);
+}
+
+Console.Clear();
+double b1 = InputNumber("Введите b1 -> ");
+double k1 = InputNumber("Введите k1 -> ");
+double b2 = InputNumber("Введите b2 -> ");
+double k2 = InputNumber("Введите k2 -> ");
+CrossPoint(b1,k1,b2,k2);
+
+
+
+
+
